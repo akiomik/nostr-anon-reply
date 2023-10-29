2f7ce67a46f9402aad8f4c7d096f5a49a90c0a27
Akiomi Kamakura <akiomik@gmail.com>
2023-10-29 18:03:28 +0900

    Add form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,80 @@
+<script lang="ts">
+  import {
+    nip19,
+    validateEvent,
+    getSignature,
+    getEventHash,
+    generatePrivateKey,
+    getPublicKey,
+    SimplePool,
+  } from 'nostr-tools';
+
+  let noteId = '';
+  let hashtag = '#makeitquote';
+  let status = '';
+
+  const relays = ['wss://yabu.me', 'wss://nos.lol', 'wss://relay-jp.nostr.wirednet.jp'];
+
+  const handleSumit = () => {
+    status = '';
+
+    const pool = new SimplePool();
+
+    const sk = generatePrivateKey();
+    console.debug('sk:', sk);
+
+    const pk = getPublicKey(sk);
+    console.debug('pk:', pk);
+
+    const { data: etag } = nip19.decode(noteId);
+
+    const ev = {
+      kind: 1,
+      created_at: Math.floor(Date.now() / 1000),
+      tags: [
+        ['e', etag],
+        ['t', hashtag.substring(1)],
+      ],
+      content: `${hashtag}`,
+      pubkey: pk,
+    };
+    ev.id = getEventHash(ev);
+    ev.sig = getSignature(ev, sk);
+    console.debug('ev:', ev, validateEvent(ev));
+
+    Promise.all(pool.publish(relays, ev))
+      .then(() => {
+        console.debug('published!');
+        status = 'published!';
+      })
+      .catch((e) => {
+        console.error(e);
+        status = 'error...';
+      });
+  };
+</script>
+
 <svelte:head>
-  <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+  <title>Nostr Anonymous Reply</title>
+  <meta name="description" content="Send nostr reply anonymously" />
 </svelte:head>
 
 <section>
-  <p>Hello, world!</p>
+  <h1>Nostr Anonymous Reply</h1>
+
+  <form on:submit={handleSumit}>
+    <label>
+      Note ID
+      <input bind:value={noteId} placeholder="note1..." minlength="63" maxlength="63" required />
+    </label>
+
+    <label>
+      Hashtag
+      <input bind:value={hashtag} placeholder="#makeitquote" required />
+    </label>
+
+    <button type="submit">send</button>
+  </form>
+
+  {status}
 </section>
